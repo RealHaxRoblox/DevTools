1425594bf45ca28595eb13448a83e613434ebaec
realrohaxexploits <94767339+realrohaxexploits@users.noreply.github.com>
2021-12-27 11:52:18 -0500

    Create FindPosition.lua

---
+++ FindPosition.lua
@@ -0,0 +1,2 @@
+local yourname="WindoesXPexe" 
+local youu=game.Workspace:FindFirstChild(yourname) print(youu.Head.Position) local function send(c,a,b,...)game.StarterGui:SetCore("SendNotification",{Title=c;Text=a;Icon="";Duration=b})end send("Done.","Do Ctrl + F9 to see your Position!",10)
